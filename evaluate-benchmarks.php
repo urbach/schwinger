af1316e8ee971d3b41854f7fb110cb18c7ef5c82
Daniel Alm <Daniel.Alm@ForumD.net>
2012-01-05 15:01:56 +0100

    evaluate-benchmarks.php: Some minor improvements to benchmark evaluation.

--- evaluate-benchmarks.php
+++ evaluate-benchmarks.php
@@ -13,19 +13,22 @@
 		$gnuplot_err = $pipes[2];
 		
 		$nMeasurements = count($pion_measurements);
+		
 		$fitc = 0.5 * ($nMeasurements);// - 1);
 		$fitstart = 0.25 * $nMeasurements;
 		$fitend = 0.75 * $nMeasurements;
 		
 		for ($i = 0; $i < $nMeasurements; $i ++)
 		{
+			$pcac_diff[$i] = 0.5 * ($pcac_measurements[($i+1)%$nMeasurements] + $pcac_measurements[($i-1+$nMeasurements)%$nMeasurements]);
+			
 			$data_pion .= "$i\t$pion_measurements[$i]\t$pion_errors[$i]\n";
 			$data_pcac .= "$i\t$pcac_measurements[$i]\t$pcac_errors[$i]\n";
-			$data_pion_pcac .= "$i\t" . $pcac_measurements[$i] / (2 * $pion_measurements[$i]) . "\n";
+			$data_pion_pcac .= "$i\t" . ($pion_measurements[$i] != 0 ? $pcac_measurements[$i] / (2 * $pion_measurements[$i]) : NAN) . "\n";
 		}
 		$fermion_mass = 0;
 		for ($i = $fitstart; $i <= $fitend; $i ++)
-			$fermion_mass += $pcac_measurements[$i] / (2 * $pion_measurements[$i]);
+			$fermion_mass += ($pion_measurements[$i] != 0 ? $pcac_measurements[$i] / (2 * $pion_measurements[$i]) : NAN);
 		//echo $fermion_mass / ($fitend - $fitstart + 1) . "\n";
 		$data_pion .= 'e';
 		$data_pcac .= 'e';
@@ -34,7 +37,7 @@
 		{
 			$m1 = $pion_measurements[$i + 1];
 			$m2 = $pion_measurements[$i];
-			$data_pion_relation .= "$i\t" . $m2 / $m1 . "\t" . sqrt(($pion_errors[$i] / $m1)^2 + ($pion_errors[$i + 1] * $m2 / ($m1^2))^2) . "\n";
+			$data_pion_relation .= "$i\t" . ($m1 != 0 ? $m2 / $m1 : NAN) . "\t" . (($m1 != 0 && $m2 != 0) ? sqrt(($pion_errors[$i] / $m1)^2 + ($pion_errors[$i + 1] * $m2 / ($m1^2))^2) : NAN) . "\n";
 		}
 		$data_pion_relation .= 'e';
 		$commands = <<<END
@@ -49,10 +52,10 @@ set output "plots/$filename.eps"
 set logscale y
 b=0.2
 c=$fitc
-f(x)=a*cosh(b*(x-c))
+f(x)=0.5*a*(exp(-b*x)+exp(-b*(2*c-x)))
 fit [$fitstart:$fitend] f(x) '-' using 1:2 via a, b
 $data_pion
-plot '-' using 1:2:3 with yerrorbars title "C_{/Symbol p}(t)", f(x) title "Linear fit" with lines, '-' using 1:2:3 with yerrorbars title "C(t) / C(t + 1)", '-' using 1:2:3 with yerrorbars title "C_{PCAC}(t)", '-' using 1:2 title "C_{PCAC}(t) / (2 * C_{/Symbol p}(t))"
+plot '-' using 1:2:3 with yerrorbars title "C_{/Symbol p}(t)", f(x) title "Fit" with lines, '-' using 1:2:3 with yerrorbars title "C(t) / C(t + 1)", '-' using 1:2:3 with yerrorbars title "C_{PCAC}(t)", '-' using 1:2 title "C_{PCAC}(t) / (2 * C_{/Symbol p}(t))"
 $data_pion
 $data_pion_relation
 $data_pcac
